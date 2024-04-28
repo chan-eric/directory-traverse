c4c7b9d57e5550b92133e044541901e29a935d4f
Eric Chan <egxchan@gmail.com>
2024-04-28 01:36:32 -0400

    inc 1

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import os
 import fnmatch
 import regex as re
 
+#directory to search
 file_path = 'c:/Users/EricChan/workspace/nas_master/src/com/nas/db/'
+
+#where the input file is located
+list_of_tables = 'c:/Users/EricChan/workspace/tables.csv'
 
 table_name_pattern = r'\b(?:SELECT|DELETE)\s+([a-zA-Z_][a-zA-Z0-9_]*)(?=\s*[;)\s])'
     #r'\b(?:table|from)\s+([a-zA-Z_][a-zA-Z0-9_]*)(?=\s*[;)\s])'
@@ -20,27 +24,41 @@ def extract_table_names(directory):
 
     table_names = set()
     for root, dir, files in os.walk(directory):
+        table_names = load_list(list_of_tables)
         print(root)
         print("")
         for file in fnmatch.filter(files, "*.java"):
             print("..." + root+file)
             current_file_path = os.path.join(root, file)
-            with open(current_file_path, 'r', encoding='utif8') as current:
+            with open(current_file_path, 'r', encoding='utf8') as current:
                 content = current.read()
-                matches = re.findall(table_name_pattern, content)
-                for match in matches:
-                    table_names.add("match: "+match)
-                    print(match)
+                # matches = re.findall(table_name_pattern, content)
+                # for match in matches:
+                #     table_names.add("match: "+match)
+                #     print(match)
+                for table in table_names:
+                    if(content.find(table) != -1):
+                        print(table)
             count = count + 1
         print()
         #with open(filename, "r"):
     print('total number of files: ', count)
 
-    print("Table Names:")
-    for table_name in table_names:
-        print(table_name)
+    # print("Table Names:")
+    # for table_name in table_names:
+    #     print(table_name)
 
+
+def load_list(filename):
+    table_names = set()
+    with open(filename) as file:
+        lines = [line.rstrip() for line in file]
+        for line in lines:
+            #print(line)
+            table_names.add(line)
+    return table_names
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    #load_list(list_of_tables)
     extract_table_names(file_path)
