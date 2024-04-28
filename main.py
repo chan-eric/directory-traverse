fec4ee4759aa065322c731d71fec4ef076694994
AzureAD\EricChan <eric.chan@tngoc.com>
2024-04-28 00:39:20 -0400

    init

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import glob
+import os
+import fnmatch
+import regex as re
 
-file_path = 'c:/Users/EricChan/workspace/nas_master/src/com/nas/'
+file_path = 'c:/Users/EricChan/workspace/nas_master/src/com/nas/db/'
+
+table_name_pattern = r'\b(?:SELECT|DELETE)\s+([a-zA-Z_][a-zA-Z0-9_]*)(?=\s*[;)\s])'
+    #r'\b(?:table|from)\s+([a-zA-Z_][a-zA-Z0-9_]*)(?=\s*[;)\s])'
 
 
-def extract_table_names(dir):
+def extract_table_names(directory):
     # Use a breakpoint in the code line below to debug your script.
     # with open(dir, 'r') as file:  # r means read, w for write
     #     print(file.read())
 
     count = 0
-    for filename in glob.iglob(dir + '**/*DAOMySQL.java', recursive=True):
-        print(filename)
-        #with open(filename, "r"):
 
-        count = count + 1
+    table_names = set()
+    for root, dir, files in os.walk(directory):
+        print(root)
+        print("")
+        for file in fnmatch.filter(files, "*.java"):
+            print("..." + root+file)
+            current_file_path = os.path.join(root, file)
+            with open(current_file_path, 'r', encoding='utif8') as current:
+                content = current.read()
+                matches = re.findall(table_name_pattern, content)
+                for match in matches:
+                    table_names.add("match: "+match)
+                    print(match)
+            count = count + 1
+        print()
+        #with open(filename, "r"):
     print('total number of files: ', count)
+
+    print("Table Names:")
+    for table_name in table_names:
+        print(table_name)
 
 
 # Press the green button in the gutter to run the script.
